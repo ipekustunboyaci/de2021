539af0f5906c449031334f607377d82c3b0fd086
ipekustunboyaci <90289632+ipekustunboyaci@users.noreply.github.com>
2021-10-10 21:43:49 +0200

    Update app.py

--- Assignment1/data_pp_container/app.py
+++ Assignment1/data_pp_container/app.py
@@ -9,13 +9,14 @@ from resources import data_cleaner
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/db_preprocessing/<table_name>', methods=['PUT'])
+@app.route('/db_preprocessing/<table_name>', methods=['POST'])
 def clean_data(table_name):
     db_api = os.environ['TRAININGDB_API']
     r = requests.get(db_api)
     j = r.json()
     df = pd.DataFrame.from_dict(j)
-    data_cleaner.clean(df)
-    return json.dumps({'message': 'data is cleaned'}, sort_keys=False, indent=4), 200
+    resp = data_cleaner.clean(df)
+    #return json.dumps({'message': 'data is cleaned'}, sort_keys=False, indent=4), 200
+    return resp
 
 app.run(host='0.0.0.0', port=5006)
